9437b298a10f441e44b0935ea1304f44798603f0
valeriodipaolo1997 <dipaolovalerio80gmail.com>
2023-10-25 14:51:46 +0200

    print hello world in page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel primi passi</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+</head>
+
+<body class="antialiased">
+
+    <h1>Hello world!</h1>
+
+</body>
+
+</html>
